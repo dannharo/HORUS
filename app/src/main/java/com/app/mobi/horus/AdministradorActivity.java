28de7b900b5a8a5418dd0c6dde8e7f290d1d4454
Daniel Haro <dannharo@hotmail.com>
2016-11-10 19:37:42 -0600

    se agrego el list de admons

--- app/src/main/java/com/app/mobi/horus/AdministradorActivity.java
+++ app/src/main/java/com/app/mobi/horus/AdministradorActivity.java
@@ -1,5 +1,6 @@
 package com.app.mobi.horus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,6 +9,8 @@ import android.widget.EditText;
 import android.view.View;
 import android.widget.Toast;
 
+import java.sql.SQLException;
+
 public class AdministradorActivity extends AppCompatActivity {
 
     Mensaje sms = new Mensaje(this);
@@ -15,6 +18,7 @@ public class AdministradorActivity extends AppCompatActivity {
     String contraseña ="123456";
     String mensaje;
     String noTelGps = "3121105454";
+    private DBManager dbmanager;
 
     EditText nombre, noTelefono;
     Button btnGuardarAdmin;
@@ -27,6 +31,12 @@ public class AdministradorActivity extends AppCompatActivity {
 
         nombre = (EditText)findViewById(R.id.editNombreAdmin);
         noTelefono= (EditText)findViewById(R.id.editTelAdmin);
+        dbmanager = new DBManager(this);
+        try {
+            dbmanager.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         btnGuardarAdmin = (Button)findViewById(R.id.btnGuardaAdmin);
         btnGuardarAdmin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -37,15 +47,24 @@ public class AdministradorActivity extends AppCompatActivity {
                     nombre.setError("El nombre es obligatorio");
                     return;
                 }
-                if(TextUtils.isEmpty(telAdminText))
+                else if(TextUtils.isEmpty(telAdminText))
                 {
                     noTelefono.setError("El número de teléfono es obligatorio");
                     return;
                 }
-                if (telAdminText.trim().length() <10)
+                else if (telAdminText.trim().length() <10)
                 {
                     noTelefono.setError("Escriba un número de teléfono válido");
                     return;
+                }
+                else
+                {
+                    final String nombre = nombreText;
+                    final String telefono = telAdminText;
+                    final int id_disp = 1;
+                    dbmanager.insertAdministrador(id_disp,telefono,nombre);
+                    Intent main = new Intent(AdministradorActivity.this,ListAdminActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(main);
                 }
                 //Manda a llamar metodo de la clase mensaje
                 mensaje = "admin" + contraseña + " " + noTelefono;
--- app/src/main/java/com/app/mobi/horus/ListAdminActivity.java
+++ app/src/main/java/com/app/mobi/horus/ListAdminActivity.java
@@ -1,17 +1,26 @@
 package com.app.mobi.horus;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.sql.SQLException;
 
 public class ListAdminActivity extends AppCompatActivity {
     ImageButton floatButtonAdmin;
     ListView listViewAdmin;
-    ArrayAdapter<String> adapter;
+    private DBManager dbmanager;
+    //ArrayAdapter<String> adapter;
+    private SimpleCursorAdapter adapter;
+    final String[] from = new String[]{HorusDB.T_A_NOMBRE};
+    final int[] to = new int[]{R.id.lvnombreAdministrador};
+
     String[] administradoresList = {"Administrador 1", "Administrador 2"};
 
     @Override
@@ -19,6 +28,18 @@ public class ListAdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_admin);
         //Muestra el botón flotante para agregar un administrador
+        dbmanager = new DBManager(this);
+        try {
+            dbmanager.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Cursor cursor = dbmanager.fetchAdministradores();
+        listViewAdmin = (ListView) findViewById(R.id.listAdmin);
+        listViewAdmin.setEmptyView(findViewById(R.id.empty1));
+        adapter = new SimpleCursorAdapter(this,R.layout.list_administradores,cursor,from,to,0);
+        adapter.notifyDataSetChanged();
+        listViewAdmin.setAdapter(adapter);
         floatButtonAdmin =(ImageButton) findViewById(R.id.imageButtonAdmin);
         floatButtonAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +50,8 @@ public class ListAdminActivity extends AppCompatActivity {
             }
         });
         //Muestra en la lista los administadores
-        listViewAdmin=(ListView)findViewById(R.id.listAdmin);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, administradoresList);
-        listViewAdmin.setAdapter(adapter);
+        //listViewAdmin=(ListView)findViewById(R.id.listAdmin);
+        //adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, administradoresList);
+        //listViewAdmin.setAdapter(adapter);
     }
 }
