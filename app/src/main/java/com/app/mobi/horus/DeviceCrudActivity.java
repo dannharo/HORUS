75e6afa6610d6124acb20bf7251147c3be131add
Daniel Haro <dannharo@hotmail.com>
2016-11-14 17:08:11 -0600

    se agrego el form paar disp

---
+++ app/src/main/java/com/app/mobi/horus/DeviceCrudActivity.java
@@ -0,0 +1,13 @@
+package com.app.mobi.horus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceCrudActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_crud);
+    }
+}
