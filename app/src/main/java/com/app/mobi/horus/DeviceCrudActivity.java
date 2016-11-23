cc7ada5d1112a18fbee01acd7fc8b6b7451b8416
Daniel Haro <dannharo@hotmail.com>
2016-11-22 20:02:03 -0600

    se agrego borrar el dispositivo

--- app/src/main/java/com/app/mobi/horus/DeviceCrudActivity.java
+++ app/src/main/java/com/app/mobi/horus/DeviceCrudActivity.java
@@ -1,7 +1,10 @@
 package com.app.mobi.horus;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +17,7 @@ import java.sql.SQLException;
 
 public class DeviceCrudActivity extends AppCompatActivity {
 
-    Button btnMapa;
+    Button btnMapa,btnEditar,btnEliminar;
     EditText textNombre,textNumero,textIMEI,textPassword;
     private DBManager dbManager;
     //Variable que almacena el id del dispositivo seleccionado
@@ -51,11 +54,33 @@ public class DeviceCrudActivity extends AppCompatActivity {
         }
         cursor.close();
         btnMapa =(Button) findViewById(R.id.btnOptMapa);
+        btnEditar = (Button) findViewById(R.id.btnOptEditar);
+        btnEliminar = (Button) findViewById(R.id.btnOptEliminar);
         btnMapa.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(DeviceCrudActivity.this, MapAct.class);
                 intent.putExtra("id_device",id);
                 startActivity(intent);
+            }
+        });
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(DeviceCrudActivity.this);
+                //Metodos del constructor
+                builder.setTitle("Eliminar Registro");
+                builder.setMessage("¿Desea borrar el registro?");
+                builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dbManager.deleteDispositivo(_id);
+                        Intent intent = new Intent(DeviceCrudActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(DeviceCrudActivity.this, "Se borró el registro", Toast.LENGTH_SHORT ).show();
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, null);
+                Dialog dialog = builder.create();
+                dialog.show();
             }
         });
 
